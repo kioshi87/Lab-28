8eba54589f476b0ec0e97720c1f6f2029d39ce3a
Kioshi <kioshimneal@msn.com>
2019-08-12 21:05:40 -0400

    Lab 28

---
+++ Data/DeckContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab28.Models
+{
+    public class DeckContext : DbContext
+    {
+        public DeckContext (DbContextOptions<DeckContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab28.Models.Deck> Deck { get; set; }
+    }
+}
